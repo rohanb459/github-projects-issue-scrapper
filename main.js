d241a80ff60862aef508dad9a1b6ab1dcf8e5bc5
Rohan Bhatia <rohanb459@gmail.com>
2022-04-28 23:36:58 +0530

    request on topics page and extracted links

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 const url = "https://github.com/topics";
 const request = require("request");
 const cheerio = require("cheerio");
+
+request(url,cb);
+
+function cb(err, response, html)
+{
+    if(err)
+    console.log(err);
+    else
+    extractHtml(html);
+}
+
+function extractHtml(html)
+{
+    let $ = cheerio.load(html);
+
+    let linkEleArr = $(".topic-box.position-relative.hover-grow.height-full.text-center.border.color-border-muted.rounded.color-bg-default.p-5 a")
+    // console.log(linkEleArr);
+    for(let i=0; i<linkEleArr.length; i++)
+    {
+        let href = $(linkEleArr[i]).attr("href");
+        console.log(href);
+    }
+}
