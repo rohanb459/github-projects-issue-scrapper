c24fdc8dc333eb2519457bcfdf4f2950dde6fe45
Rohan Bhatia <rohanb459@gmail.com>
2022-04-30 15:47:38 +0530

    repos link fetched

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const url = "https://github.com/topics";
 const request = require("request");
 const cheerio = require("cheerio");
-
+// const getReposPage = require("./getReposPage");
 request(url,cb);
 
 function cb(err, response, html)
@@ -21,6 +21,41 @@ function extractHtml(html)
     for(let i=0; i<linkEleArr.length; i++)
     {
         let href = $(linkEleArr[i]).attr("href");
-        console.log(href);
+        let fullLink = "https://github.com"+href;
+        let topic = href.split("/").pop();
+        // console.log(fullLink);
+        // console.log("````````````````````````");
+        getReposPageHtml(fullLink, topic);
+    }
+}
+
+function getReposPageHtml(url, topic)
+{
+    request(url,cb);
+    function cb(err, response, html)
+    {
+        if(err)
+        console.log(err);
+        else
+        getReposLink(html, topic);
+    }
+
+
+    function getReposLink(html)
+    {
+        let $ = cheerio.load(html);
+        let anchorsArr = $("h3 a");
+        console.log(topic);
+        for(let i =0; i<anchorsArr.length; i++)
+        {
+            if(i%2!=0)
+            {
+                let link = $(anchorsArr[i]).attr("href");
+                // console.log(link);
+                let fullLink = `https://github.com${link}/issues`;
+                console.log(fullLink);
+            }
+        }
+        console.log("``````````````````");
     }
 }
